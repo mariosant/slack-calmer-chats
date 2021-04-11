a339b90ef771ce6beff668d684cbc1d82b069525
Marios Antonoudiou <mariosant@sent.com>
2021-04-11 21:33:31 +0000

    Convert project to typescript

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,14 @@
-import bolt from "@slack/bolt";
-
-const { App } = bolt;
+import { App } from "@slack/bolt";
 
 const app = new App({
   token: process.env.SLACK_OAUTH_TOKEN,
   signingSecret: process.env.SLACK_SIGNING_SECRET,
 });
 
-app.event("message", (props) => {
+app.event("message", async (props) => {
   console.log(JSON.stringify(props.body, null, 2));
 });
 
-app.start(process.env.PORT ?? 3000).then(() => console.log("⚡️ Bot started"));
+app
+  .start(Number(process.env.PORT) || 3000)
+  .then(() => console.log("⚡️ Bot started"));
