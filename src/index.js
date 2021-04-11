eb52f775b6c31890a7b6e7216608f7acc7b9ba9c
Marios Antonoudiou <mariosant@sent.com>
2021-04-11 20:59:35 +0000

    Connect to slack

--- src/index.js
+++ src/index.js
@@ -7,4 +7,8 @@ const app = new App({
   signingSecret: process.env.SLACK_SIGNING_SECRET,
 });
 
+app.event("message", (props) => {
+  console.log(JSON.stringify(props.body, null, 2));
+});
+
 app.start(process.env.PORT ?? 3000).then(() => console.log("⚡️ Bot started"));
